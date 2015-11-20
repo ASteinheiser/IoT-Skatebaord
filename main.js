820604b73afb3893ccc4bb55ac8fbb8020dcaa39
ASteinheiser <asteinhe@asu.edu>
2015-11-20 11:39:52 -0700

    added lodash

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var meshblu = require('meshblu');
 var meshbluJSON = require('./meshblu.json');
 var five = require("johnny-five");
+var _ = require('lodash');
 
 var uuid    = meshbluJSON.uuid;
 var token   = meshbluJSON.token;
@@ -13,11 +14,20 @@ var conn = meshblu.createConnection({
 var MESSAGE_SCHEMA = {
   "type": 'object',
   "properties": {
-    "example": {
-      "type": "string"
+    "wheelDiameter": {
+      "type": "integer"
     }
   }
 };
+
+var throttledEmit = _.throttle(function(payload){
+  conn.message({
+    "devices": "*",
+    "payload": {
+      payload
+    }
+  });
+}, 500);
 
 conn.on('notReady', function(data){
   console.log('UUID FAILED AUTHENTICATION!');
@@ -52,27 +62,19 @@ conn.on('ready', function(data){
 
     hallEffect.on("change", function() {
       if (this.value == 0) {
-        distance += (70*Math.PI)/1000;
+        distance += ((self.options.wheelDiameter)*Math.PI)/1000;
         console.log("total distance: " + Math.round(distance * 100) / 100 + " meters");
-        conn.message({
-          "devices": "*",
-          "payload": {
-            "distance": Math.round(distance * 100) / 100
-          }
-        });
+
+        throttledEmit("distance": Math.round(distance * 100) / 100);
       }
     });
 
     imu.on("change", function() {
       if (this.accelerometer.y >= 0.9){
         console.log("------------ Push in progress!! ------------");
-        conn.message({
-          "devices": "*",
-          "payload": {
-            "action": "push",
-            "accel": this.accelerometer.y
-          }
-        });
+
+        throttledEmit("action": "push");
+        throttledEmit("accel": this.accelerometer.y);
       }
     });
   });
