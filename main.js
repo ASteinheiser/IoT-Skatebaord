733094fae0c9f0eb32513897cb8040d2befcd16c
ASteinheiser <asteinhe@asu.edu>
2016-05-28 09:20:26 -0700

    trying a new accelerometer

--- main.js
+++ main.js
@@ -1,45 +1,9 @@
 var meshblu = require('meshblu');
 var meshbluJSON = require('./meshblu.json');
 var five = require("johnny-five");
-var _ = require('lodash');
-var Stats = require('fast-stats').Stats;
-
-var uuid = meshbluJSON.uuid;
-var token = meshbluJSON.token;
 
 var edison = new five.Board({
   port: "/dev/ttyMFD1"
-});
-
-var push = 0, distance = 0, wheelDiameter = 0, index = 0;
-var sampleSize = 5;
-var sample = new Stats();
-var posPushThreshold = 0.17;
-var negPushThreshold = (-0.17);
-
-var skateData = {
-  pushes: 0,
-  distance: 0
-};
-
-function sendMessage(message){
-  conn.message({
-    "devices": ["*"],
-    "payload": message
-  });
-};
-
-function resetData(){
-  skateData = {pushes: 0, distance: 0};
-  distance = 0;
-  push = 0;
-  index = 0;
-  sample.reset();
-};
-
-var conn = meshblu.createConnection({
-  "uuid": uuid,
-  "token": token
 });
 
 var MESSAGE_SCHEMA = {
@@ -60,6 +24,21 @@ var OPTIONS_SCHEMA = {
     }
   }
 };
+
+function sendMessage(message){
+  conn.message({
+    "devices": ["*"],
+    "payload": message
+  });
+};
+
+var uuid = meshbluJSON.uuid;
+var token = meshbluJSON.token;
+
+var conn = meshblu.createConnection({
+  "uuid": uuid,
+  "token": token
+});
 
 conn.on('notReady', function(data){
   console.log('UUID FAILED AUTHENTICATION!');
@@ -88,41 +67,26 @@ conn.on('ready', function(data){
 
   edison.on("ready", function() {
 
-    var reedSwitch = new five.Sensor.Digital(12);
-    var accelerometer = new five.IMU({controller: "MPU6050"});
+    var zAccel = new five.Sensor.Analog(5);
+    var yAccel = new five.Sensor.Analog(6);
+    var xAccel = new five.Sensor.Analog(7);
 
     conn.on('message', function(message){
       if (message.reset == true) {
-        resetData();
+        console.log('reset');
       }
     });
 
-    reedSwitch.on("change", function() {
-      if (this.value == 1) {
-        distance += (wheelDiameter*Math.PI)/1000;
-        skateData.distance = distance;
-        sendMessage(skateData);
-      }
+    zAccel.on("change", function() {
+      console.log("Z accelerometer: ", this.value)
     });
 
-    accelerometer.on("change", function() {
-      if (index < sampleSize) {
-        sample.push(this.accelerometer.y);
-        index ++;
-      } else {
-        sample.shift();
-        sample.push(this.accelerometer.y);
+    yAccel.on("change", function() {
+      console.log("Y accelerometer: ", this.value)
+    });
 
-        var range = sample.range();
-
-        if (range[1] > posPushThreshold && range[0] < negPushThreshold) {
-          push ++;
-          skateData.pushes = push;
-          sendMessage(skateData);
-          index = 0;
-          sample.reset();
-        }
-      }
+    xAccel.on("change", function() {
+      console.log("X accelerometer: ", this.value)
     });
   });
 });
