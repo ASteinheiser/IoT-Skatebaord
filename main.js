9acd6ce2030d3007c7a3b91fc5975cbc77877372
ASteinheiser <asteinhe@asu.edu>
2016-05-28 15:40:53 -0700

    expects a function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var meshblu = require('meshblu');
-var meshbluJSON = require('./meshblu.json');
-var five = require("johnny-five");
-var _ = require("lodash");
+var meshblu = require('meshblu')
+var meshbluJSON = require('./meshblu.json')
+var five = require("johnny-five")
+var _ = require("lodash")
 
 // var MESSAGE_SCHEMA = {
 //   "type": "object",
@@ -11,7 +11,7 @@ var _ = require("lodash");
 //       "default": false
 //     }
 //   }
-// };
+// }
 //
 // var OPTIONS_SCHEMA = {
 //   "type": "object",
@@ -20,44 +20,48 @@ var _ = require("lodash");
 //       "type": "integer"
 //     }
 //   }
-// };
+// }
 
 function sendMessage(message){
   conn.message({
     "devices": ["*"],
     "payload": message
-  });
-};
+  })
+}
+
+function log(message){
+  console.log(message)
+}
 
 var edison = new five.Board({
   port: "/dev/ttyMFD1"
-});
+})
 
-var uuid = meshbluJSON.uuid;
-var token = meshbluJSON.token;
+var uuid = meshbluJSON.uuid
+var token = meshbluJSON.token
 
 var conn = meshblu.createConnection({
   "uuid": uuid,
   "token": token
-});
+})
 
 conn.on('notReady', function(data){
-  console.log('UUID FAILED AUTHENTICATION!', data);
-  sendMessage("UUID FAILED AUTH: ", data);
-});
+  console.log('UUID FAILED AUTHENTICATION!', data)
+  sendMessage("UUID FAILED AUTH: ", data)
+})
 
 conn.on('config', function(device){
-  // wheelDiameter = device.options.wheelDiameter;
-});
+  // wheelDiameter = device.options.wheelDiameter
+})
 
 conn.on('ready', function(data){
-  console.log('UUID AUTHENTICATED!');
-  console.log(data);
+  console.log('UUID AUTHENTICATED!')
+  console.log(data)
   sendMessage('Device Ready')
 
   conn.whoami({}, function(device){
-    // wheelDiameter = device.options.wheelDiameter;
-  });
+    // wheelDiameter = device.options.wheelDiameter
+  })
 
   conn.update({
     "uuid": uuid,
@@ -65,30 +69,30 @@ conn.on('ready', function(data){
     // "optionsSchema": OPTIONS_SCHEMA,
     "type": "device:iot-skateboard",
     "logoUrl": "https://s3-us-west-2.amazonaws.com/octoblu-icons/device/iot-skateboard.svg"
-  });
+  })
 
   edison.on("ready", function() {
 
-    var zAccel = new five.Sensor.Analog(0);
-    var yAccel = new five.Sensor.Analog(6);
-    var xAccel = new five.Sensor.Analog(7);
+    var zAccel = new five.Sensor.Analog(0)
+    var yAccel = new five.Sensor.Analog(6)
+    var xAccel = new five.Sensor.Analog(7)
 
     conn.on('message', function(message){
       // if (message.reset == true) {
-      //   console.log('reset');
+      //   console.log('reset')
       // }
-    });
+    })
 
     zAccel.on("change", function() {
-      _.throttle(console.log("Z accelerometer: ", this.value), 500);
-    });
+      _.throttle(log("Z accelerometer: " + this.value), 500)
+    })
 
     yAccel.on("change", function() {
-      _.throttle(console.log("Y accelerometer: ", this.value), 500);
-    });
+      _.throttle(log("Y accelerometer: " + this.value), 500)
+    })
 
     xAccel.on("change", function() {
-      _.throttle(console.log("X accelerometer: ", this.value), 500);
-    });
-  });
-});
+      _.throttle(log("X accelerometer: " + this.value), 500)
+    })
+  })
+})
